22bbceeac2303a717e625de0ad5900ddad548caf
Huy <A3huynguyen@gmail.com>
2021-11-28 13:58:22 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList, ScrollView, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { Appbar, TextInput, Button } from "react-native-paper";
+import Todo from "./components/todo";
 
-export default function App() {
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const ref = firestore().collection("todos");
+  console.log(ref);
+
+  async function addTodo() {
+    await ref.add({
+      title: todo,
+      complete: false,
+    });
+    setTodo("");
+  }
+
+  useEffect(() => {
+    return ref.onSnapshot((querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        const { title, complete } = doc.data();
+        list.push({
+          id: doc.id,
+          title,
+          complete,
+        });
+      });
+      setTodos(list);
+      if (loading) {
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Appbar>
+        <Appbar.Content title={"TODOs List"} />
+      </Appbar>
+      <FlatList
+        style={{ flex: 1 }}
+        data={todos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Todo {...item} />}
+      />
+      <TextInput
+        label={"New Todo"}
+        value={todo}
+        onChangeText={(text) => setTodo(text)}
+      />
+      <Button onPress={addTodo}>Add TODO</Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
